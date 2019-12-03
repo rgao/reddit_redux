ee8af589c514b1a1256913154b5fc7f321097008
rgao <xinghoshi@gmail.com>
2019-12-02 23:03:50 -0800

    added docstrings to server file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// main web app
 const express = require('express')
 const app = express()
 
+// logging middleware and json parser
 const logger = require('morgan');
 app.use(logger('dev'));
 
@@ -8,10 +10,12 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// for heroku deployment
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 };
 
+// using the appropriate connection string and starting up a client instance with connection to postgreSQL
 const env = process.env.NODE_ENV || 'test';
 const envConfig = require('./server/config/config.js')[env];
 const Client = require('pg').Client;
@@ -22,9 +26,11 @@ const client = new Client({
 
 client.connect();
 
+// routing
 const routes = require("./server/routes/index.js");
 app.use(routes);
 
+// client
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, function () {
